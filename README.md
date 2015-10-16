4815d2465801013a742ac15ccfb1ff0e9402f3e5
Kent <register@gividen.com>
2015-10-15 23:39:09 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # com.netsmartcompany.floatingActionButton
-Alloy widget for a simple floating action button
+
+A simple Floating Action Button for Titanium Alloy Widget. Based off of http://gitt.io/component/be.k0suke.tifab.
+
+## Usage
+
+**Controller**
+
+```javascript
+$.addFolderFab.onClick(function(e) {
+	addFolderClicked();
+});
+$.index.open();
+```
+
+**View**
+
+```xml
+<Alloy>
+	<Window>
+		<Widget id="addFolderFab" class="fab" src="com.netsmartcompany.floatingActionButton" />
+	</Window>
+</Alloy>
+```
+
+**Style**
+
+```javascript
+'.fab': {
+    zIndex: 1000,
+    top: 20,
+    right: 10,
+    width: 60,
+    height: 60,
+    backgroundColor: '#cc2ecc40',
+    borderWidth: 1,
+    borderColor: '#ececec',
+    title: '\uf067',
+    font: {
+        fontFamily: 'FontAwesome',
+        fontSize: 40
+    },
+    color: '#ffffff'
+}
+```
+
+![iOS](ios.png)
+## License
+
+MIT
---
+++ controllers/widget.js
@@ -0,0 +1,51 @@
+var args = arguments[0] || {};
+
+var WIDTH = 60,
+	HEIGHT = 60;
+
+var widget = {
+		top: _.has(args, 'top') ? args.top : null,
+		right: _.has(args, 'right') ? args.right : null,
+		bottom: _.has(args, 'bottom') ? args.bottom : null,
+		left: _.has(args, 'left') ? args.left : null,
+		width: _.has(args, 'width') ? args.width : WIDTH,
+		height: _.has(args, 'height') ? args.height : HEIGHT
+	},
+	circle = {
+		width: _.has(args, 'width') ? args.width : WIDTH,
+		height: _.has(args, 'height') ? args.height : HEIGHT,
+		borderRadius: _.has(args, 'width') ? args.width / 2 : WIDTH / 2,
+		borderWidth: _.has(args, 'width') ? args.width / 2 : WIDTH / 2,
+		borderColor: _.has(args, 'backgroundColor') ? args.backgroundColor : '#ccfafafa'
+	},
+	title = {
+		width: _.has(args, 'width') ? args.width : WIDTH,
+		height: _.has(args, 'height') ? args.height : HEIGHT,
+		text: _.has(args, 'title') ? args.title : '',
+		color: _.has(args, 'color') ? args.color : '#808080',
+		font: _.has(args, 'font') ? args.font : {},
+		textAlign: _.has(args, 'textAlign') ? args.textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+		verticalAlign: _.has(args, 'verticalAlign') ? args.verticalAlign : Ti.UI.TEXT_VERTICAL_ALIGNMENT_CENTER
+	};
+
+$.widget.applyProperties(widget);
+$.circle.applyProperties(circle);
+$.title.applyProperties(title);
+
+// This will hold our callback
+var onClickCallback;
+
+// The button has been clicked, call callback
+function buttonClicked(e) {
+	if(typeof(onClickCallback) === 'function') {
+		onClickCallback();
+	}
+}
+
+// Assign our callback
+function onClick(callback) {
+	onClickCallback = callback;
+}
+
+// Make the onClick function public
+exports.onClick = onClick;
